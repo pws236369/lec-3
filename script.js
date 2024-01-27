2d36c78428c668300edf2706ab18e9aa770096d4
nissano <nissano@wix.com>
2024-01-27 16:53:49 +0200

    more fixes

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 document.getElementById('add-shark-btn').addEventListener('click', function () {
   // Retrieve shark details from input fields
-  var imgUrl = document.getElementById('shark-img-url').value;
-  var sharkName = document.getElementById('shark-name').value;
-  var sharkDescription = document.getElementById('shark-description').value;
+  const imgUrl = document.getElementById('shark-img-url').value;
+  const sharkName = document.getElementById('shark-name').value;
+  const sharkDescription = document.getElementById('shark-description').value;
 
   // Create the new shark div
-  var newSharkDiv = document.createElement('div');
-  // Create the new shark div
-  var newSharkDiv = document.createElement('div');
+  const newSharkDiv = document.createElement('div');
 
   // Create and append the image
-  var image = document.createElement('img');
+  const image = document.createElement('img');
   image.src = imgUrl;
   image.alt = sharkName;
   newSharkDiv.appendChild(image);
 
   // Create and append the h2 element
-  var title = document.createElement('h2');
+  const title = document.createElement('h2');
   title.textContent = sharkName;
   newSharkDiv.appendChild(title);
 
   // Create and append the paragraph
-  var description = document.createElement('p');
+  const description = document.createElement('p');
   description.textContent = sharkDescription;
   newSharkDiv.appendChild(description);
 
