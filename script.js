b3de858f3e089ff1726493e33a57a53718bbde4d
nissano <nissano@wix.com>
2024-01-27 16:50:22 +0200

    fixes

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ document.getElementById('add-shark-btn').addEventListener('click', function () {
 
   // Create the new shark div
   var newSharkDiv = document.createElement('div');
-  newSharkDiv.innerHTML = `
-      <img src="${imgUrl}" alt="${sharkName}" />
-      <h2>${sharkName}</h2>
-      <p>${sharkDescription}</p>
-    `;
+  // Create the new shark div
+  var newSharkDiv = document.createElement('div');
+
+  // Create and append the image
+  var image = document.createElement('img');
+  image.src = imgUrl;
+  image.alt = sharkName;
+  newSharkDiv.appendChild(image);
+
+  // Create and append the h2 element
+  var title = document.createElement('h2');
+  title.textContent = sharkName;
+  newSharkDiv.appendChild(title);
+
+  // Create and append the paragraph
+  var description = document.createElement('p');
+  description.textContent = sharkDescription;
+  newSharkDiv.appendChild(description);
 
   // Append the new shark to the container
   document.getElementById('sharks-container').appendChild(newSharkDiv);
